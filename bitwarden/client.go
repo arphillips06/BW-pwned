bd24a656208e0b1782f954f769c76622285bedd4
Ross <adam.ross.phillips@gmail.com>
2025-11-15 19:06:36 +0000

    removed hardcoded url - put in a const

--- bitwarden/client.go
+++ bitwarden/client.go
@@ -5,23 +5,26 @@ import (
 	"bw-hibp-check/models"
 	"fmt"
 	"log"
+	"path"
 )
+
+const bwBaseURL = "http://localhost:8087"
 
 func GetStatus() (*models.VaultStatus, error) {
 	var resp models.VaultStatus
-	err := helper.DoRequest("GET", "http://localhost:8087/status", nil, &resp)
-	if err != nil {
-		return nil, err
+	url := path.Join(bwBaseURL, "status")
+	if err := helper.DoRequest("GET", url, nil, &resp); err != nil {
+		return nil, fmt.Errorf("status request failed: %w", err)
 	}
 	return &resp, nil
 }
 
 func UnlockVault(password string) (*models.UnlockResponse, error) {
 	var resp models.UnlockResponse
-	err := helper.DoRequest("POST", "http://localhost:8087/unlock",
-		models.UnlockRequest{Password: password}, &resp)
-	if err != nil {
-		return nil, err
+	url := path.Join(bwBaseURL, "unlock")
+	body := models.UnlockRequest{Password: password}
+	if err := helper.DoRequest("POST", url, body, &resp); err != nil {
+		return nil, fmt.Errorf("unlock vault failed: %w", err)
 	}
 	log.Printf("Unlocked: %v | Message: %s", resp.Success, resp.Data.Title)
 	return &resp, nil
@@ -29,9 +32,9 @@ func UnlockVault(password string) (*models.UnlockResponse, error) {
 
 func GetItem(id string) (*models.BitwardenItemResponse, error) {
 	var resp models.BitwardenItemResponse
-	url := fmt.Sprintf("http://localhost:8087/object/item/%s", id)
+	url := path.Join(bwBaseURL, "object", "item", id)
 	if err := helper.DoRequest("GET", url, nil, &resp); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("get item %s failed: %w", id, err)
 	}
 	return &resp, nil
 }
